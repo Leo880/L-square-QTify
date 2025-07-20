2aec12627f3aff68cb1981006ab4911d9099c8fd
unknown <amit.dcosta12@gmail.com>
2025-07-20 16:49:24 +0530

    fix: updated module1

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,13 +4,14 @@
 export default function Logo() {
   return <img src={LogoImage} alt="logo" width={67} />;
 }*/
-import styles from "./Logo.module.css";
-import LogoImage from "../../assets/logo.png";
+/*import styles from "./Logo.module.css";
+import LogoImage from "../../assets/logo.png";*/
 
 /*export default function Logo({ className }) {
   return <img src={LogoImage} alt="logo" width={67} className={className} />;
 }*/
 
+/*
 export default function Logo({ className = "" }) {
   return (
     <img
@@ -20,5 +21,22 @@ export default function Logo({ className = "" }) {
       width={67}
       height={40} // optional: control height for alignment
     />
+  );
+}*/
+
+import styles from "./Logo.module.css";
+import LogoImage from "../../assets/logo.png";
+
+export default function Logo({ className = "" }) {
+  return (
+    <div className={styles.logoWrapper}>
+      <img
+        className={`${styles.logo} ${className}`}
+        src={LogoImage}
+        alt="logo"
+        width={67}
+        height={40}
+      />
+    </div>
   );
 }
