75731685a1390d26fad7242df1425c1971037bc7
unknown <amit.dcosta12@gmail.com>
2025-07-20 17:16:48 +0530

    fix: updated module1

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -24,6 +24,7 @@ export default function Logo({ className = "" }) {
   );
 }*/
 
+/*
 import styles from "./Logo.module.css";
 import LogoImage from "../../assets/logo.png";
 
@@ -32,6 +33,23 @@ export default function Logo({ className = "" }) {
     <div className={styles.logoWrapper}>
       <img
         className={`${styles.logo} ${className}`}
+        src={LogoImage}
+        alt="logo"
+        width={67}
+        height={40}
+      />
+    </div>
+  );
+}
+*/
+import styles from "./Logo.module.css";
+import LogoImage from "../../assets/logo.png";
+
+export default function Logo({ className = "" }) {
+  return (
+    <div className={`${styles.logoWrapper} ${className}`}>
+      <img
+        className={styles.logo}
         src={LogoImage}
         alt="logo"
         width={67}
